ac4ea295829db10cb7c5356009e804787fc98634
scary987 <tobyas987@googlemail.com>
2020-03-21 19:14:22 +0100

    making an account successfully works

--- swtdatabaseinterface.cpp
+++ swtdatabaseinterface.cpp
@@ -24,15 +24,15 @@ sql::Connection *con;
 int register_function(string username, string passphrase, string vorname, string nachname, string birthdate,string emailname,string emailhost){
 	 try {
 
-  sql::Statement *stmt;
+  sql::Statement *stmt; 
   sql::ResultSet *res;
-
+  sql::ResultSet *res1;
   /* Create a connection */
   driver = get_driver_instance();
   con = driver->connect("tcp://localhost:3306", "scary__", "monsterssupercreeps");
   /* Connect to the MySQL test database */
   con->setSchema("swtproject");
-  string statement="Select count(username) as number  from signin where username="+username;
+  string statement="Select count(username) as number  from signin where username='"+username+"'";
   stmt = con->createStatement();
   cout<<"Running: "<<statement<<endl; 
   res = stmt->executeQuery(statement); 
@@ -44,27 +44,33 @@ int register_function(string username, string passphrase, string vorname, string
 	else{
 	stmt = con->createStatement();
 	cout<<"Running Execute: "<<statement<<endl;
-	statement="Insert into singin(username,password) values ("+username+","+passphrase+") ";
+	statement="Insert into signin(username,password) values ('"+username+"','"+passphrase+"') ";
   	stmt->execute(statement);
+	stmt = con-> createStatement(); 
+        statement="call loginexists('"+username+"',@a,@b)";
 	cout<<"Running: "<<statement<<endl;
-        statement="select @a as a from (call loginexists(@a,@b,"+username+"))";
-	stmt=con->createStatement();
-	cout<<"Running: "<<statement<<endl;
-	res = stmt->executeQuery(statement); 
-	string id = res->getString("a");
-
+	stmt->execute(statement);
+	statement="select id from signin where username='"+username+"'";
+	cout<<"Running: "+statement<<endl;
+	stmt=con->createStatement(); 
+	res1 = stmt->executeQuery(statement);
+        res1->next(); 	
+	int id = res1->getInt("id");
+	cout<<"The generated ID: "+id <<endl;	
 
 
 
 
 	cout<<"Now inserting into users: "<<endl;
-	statement="Insert into users(id,vorname,nachname,birthdate,emailname,emailhost) values ("+id+","+ vorname+ ","+ nachname+","+ birthdate+ ","+ emailname+","+emailhost+")";
-	res = stmt->executeQuery(statement); 
-
+	statement="Insert into users(id,vorname,nachname,birthdate,emailname,emailhost) values ("+ to_string(id)+",'"+ vorname+ "','"+ nachname+"','"+ birthdate+ "','"+ emailname+"','"+emailhost+"')";
+	 cout<<"Running: "+ statement<<endl; 
+	 stmt->execute(statement); 
+	
+	 cout<<"You have successfully registered :)\n"; 
 
 
 	}	
-    return res->getInt("number");
+    return 0 ;
         
     /*cout << "\t... MySQL says it again: ";
     /* Access column fata by numeric offset, 1 is the first column 
@@ -72,8 +78,10 @@ int register_function(string username, string passphrase, string vorname, string
   }
   
   delete res;
+  delete res1; 
   delete stmt;
   delete con;
+
 
 } catch (sql::SQLException &e) {
   cout << "# ERR: SQLException in " << __FILE__;
@@ -95,25 +103,27 @@ int  login_function(string username, string passphrase){
   con = driver->connect("tcp://localhost:3306", "scary__", "monsterssupercreeps");
   /* Connect to the MySQL test database */
   con->setSchema("swtproject");
-  string statement="Select count(username) as number  from signin where username='"+username+"' and password='" +passphrase+"'";
+  string statement="Select count(username),id as number,id  from signin where username='"+username+"' and password='" +passphrase+"'";
   stmt = con->createStatement();
   res = stmt->executeQuery(statement); 
   while (res->next()) {
     //cout << "\t... MySQL replies: ";
     /* Access column data by alias or column name */
     //cout << res->getInt("number") << endl;
-	if(!res->getInt("number"))cout<<"Either your username or passphrase is not correct."; 
-    return res->getInt("number");
-        
+	if(!res->getInt("number")){cout<<"Either your username or passphrase is not correct.";
+       return 0;	} 
+	cout<<"You're now logged in :)\n"; 
+    return res->getInt("id");
+	} 
     /*cout << "\t... MySQL says it again: ";
     /* Access column fata by numeric offset, 1 is the first column 
     cout << res->getString(1) << endl; */
-  }
+  
   delete res;
   delete stmt;
-  delete con;
+  delete con;}
 
-} catch (sql::SQLException &e) {
+  catch (sql::SQLException &e) {
   cout << "# ERR: SQLException in " << __FILE__;
   cout << "(" << __FUNCTION__ << ") on line "<< __LINE__ << endl;
   cout << "# ERR: " << e.what();
@@ -132,13 +142,11 @@ int main(int argc, char *argv[])
   bool user = 0;
   string username, password, vorname,nachname,birthdate,emailname,emailhost;   
   for(int i = 0; i<argc; i++)
-  {       cout<<argv[i]<<endl; 
+  {       printf("%s\n",argv[i]); 
 	 if(argv[i][0]=='-'){
-	  printf("%s\n",argv[i]);
-    if(argv[i]=="-u") {
-	    user = true;
-	    i++; 
-	    if (argv[i]!= NULL) username=argv[i]; 
+		 if(argv[i][1]=='u') {
+			 i++;
+			 if (argv[i]!= NULL) username=argv[i]; 
 	    else{cout<<"Error: no provided username! Abandoning process..\n";
 		    return 1;}  }
     if(argv[i][1]=='l') login = true;
@@ -159,7 +167,7 @@ int main(int argc, char *argv[])
 		    cout<<"Error: provided name is not complete! Abandoning process..\n";
 		    return 1;} }
     if(argv[i][1]=='b') { i++;
-	    if(argv[i]!=NULL&&argv[i][0]=='-') birthdate = argv[i]; 
+	    if(argv[i]!=NULL) birthdate = argv[i]; 
 	    else{
 		    cout<<"Error: no provided birthdate! Abandoning process..\n";
 		    return 1;} 
@@ -184,7 +192,8 @@ if(argv[i][1]=='e') {
   /* checking for correct commandline inputs*/
 cout << endl;
 //cout << "Running 'SELECT 'Hello World!' AS _message'..." << endl;
-//if(regist)  register_function(username,password);
+//cout << username+" "+password+" "+birthdate+"\n";
+if(regist)  register_function(username,password,vorname,nachname,birthdate,emailname,emailhost);
 if(login)  login_function(username,password);
 if(!regist && !login){cout<<"You need to call this function with either the login parameter (-l) or the register parameter (-r)\n Be sure to enter the birthdate in year-month-day notation\n"; return 0;  }
 
